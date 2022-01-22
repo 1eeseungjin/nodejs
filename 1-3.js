474e1be9a048262ef9c5a82c7edda98df7e9a43b
1eeseungjin <dltmdwls6766@hanyang.ac.kr>
2022-01-22 15:30:58 +0900

    Initial commit

---
+++ 1-3.js
@@ -0,0 +1,12 @@
+console.log("\\    /\\")
+console.log(" )  ( ')")
+console.log("(  /  ) ")
+console.log("\\(__)|")
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+
+let a = ["\\    /\\"," )  ( ')","(  /  ) "," \\(__)|"]
+console.log(a.join("\n"))
